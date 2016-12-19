812de55f6a861e481e8bb634fe85ef947bf38add
Johannes Donath <johannesd@torchmind.com>
2016-12-19 14:52:12 +0100

    Altered the previous abstract mojo definition to be a bit more generic.

--- src/main/java/org/basinmc/maven/plugins/minecraft/AbstractMinecraftMojo.java
+++ src/main/java/org/basinmc/maven/plugins/minecraft/AbstractMinecraftMojo.java
@@ -16,333 +16,108 @@
  */
 package org.basinmc.maven.plugins.minecraft;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.installer.ArtifactInstallationException;
-import org.apache.maven.artifact.installer.ArtifactInstaller;
-import org.apache.maven.artifact.metadata.ArtifactMetadata;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.execution.MavenSession;
-import org.apache.maven.model.License;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.Organization;
-import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Optional;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * <strong>Abstract Mojo</strong>
- *
- * Provides a base mojo implementation which provides access to certain configuration resources.
+ * Provides a base Maven Mojo implementation in order to share configuration properties and
+ * component references.
  *
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
+@Immutable
+@ThreadSafe
 public abstract class AbstractMinecraftMojo extends AbstractMojo {
 
-        /**
-         * <strong>Mapped Suffix</strong>
-         *
-         * Specifies an artifact suffix to append to all mapped artifacts.
-         */
-        public static final String MAPPED_SUFFIX = "-mapped";
-        /**
-         * <strong>Minecraft Group ID</strong>
-         *
-         * Specifies the group identifier cached artifacts will be installed to.
-         */
-        public static final String MINECRAFT_GROUP_ID = "org.basinmc.minecraft";
-        /**
-         * <strong>Source Suffix</strong>
-         *
-         * Specifies an artifact suffix to append to all source artifacts.
-         */
-        public static final String SOURCE_SUFFIX = "-source";
-        @Component
-        private ArtifactFactory artifactFactory;
-        @Component
-        private ArtifactInstaller artifactInstaller;
-        @Component
-        private ArtifactResolver artifactResolver;
-        /**
-         * Specifies the game version to download and map.
-         */
-        @Parameter(defaultValue = "1.9.4")
-        protected String gameVersion;
-        /**
-         * Specifies the MCP mappings version to apply.
-         */
-        @Parameter(defaultValue = "snapshot-20160601")
-        protected String mcpVersion;
-        /**
-        * Determines whether to use unstable MCP csv files
-        */
-        @Parameter(defaultValue = "false")
-        protected boolean useLiveMCP;
-        /**
-         * Specifies a list of modules to download decompile and remap.
-         * Valid values are: client, server
-         */
-        @Parameter(defaultValue = "server")
-        protected String module;
-        /**
-         * Specifies the directory to read patches from/store patches in.
-         */
-        @Parameter(defaultValue = "${project.basedir}/src/minecraft/patch")
-        protected File patchDirectory;
-        /**
-         * Stores a reference to the project's configuration.
-         */
-        @Parameter(property = "project", required = true, readonly = true)
-        protected MavenProject project;
-        @Parameter(defaultValue = "${session}", readonly = true)
-        private MavenSession session;
-        /**
-         * Specifies the source output directory.
-         */
-        @Parameter(defaultValue = "${project.basedir}/src/minecraft/java")
-        protected File sourceDirectory;
+    // <editor-fold desc="Maven Components">
+    @Parameter(defaultValue = "${session}", readonly = true)
+    private MavenSession session;
+    // </editor-fold>
 
-        /**
-         * Downloads the artifact in case no local cached version could be found.
-         *
-         * @param artifactPath a temporary artifact path.
-         * @param urls         an array of URLs that will be attempted in order. If one returns 200, it will be downloaded and the method will return.
-         * @throws MojoFailureException when an error occurs while attempting to fetch an artifact.
-         * @return whether the artifact was successfully downloaded or not
-         */
-        protected boolean downloadArtifact(@Nonnull Path artifactPath, @Nonnull String... urls) throws MojoFailureException {
-                for (int attempt = 0; attempt < urls.length; attempt++) {
-                        String url = urls[attempt];
-                        this.getLog().info("Retrieving " + url);
+    // <editor-fold desc="Configuration Properties">
+    @Parameter(required = true)
+    private String gameVersion;
+    @Parameter(required = true)
+    private String mappingVersion;
+    @Parameter(required = true)
+    private String module;
+    @Parameter(defaultValue = "${project.basedir}/src/minecraft/patch", required = true)
+    private String patchDirectory;
+    @Parameter(defaultValue = "${project.basedir}/src/minecraft/java", required = true)
+    private String sourceDirectory;
+    @Parameter(defaultValue = "${project.basedir}/src/minecraft/resource", required = true)
+    private String resourceDirectory;
+    // </editor-fold>
 
-                        try {
-                                HttpURLConnection connection = (HttpURLConnection) (new URL(url)).openConnection();
+    // <editor-fold desc="Component Getters">
+    @Nonnull
+    protected MavenSession getSession() {
+        return this.session;
+    }
+    // </editor-fold>
 
-                                if (connection.getResponseCode() == 404) {
-                                        this.getLog().info("Got 404 when retrieving " + url + ", moving to next in list...");
-                                        continue;
-                                } else if (connection.getResponseCode() == 200) {
-                                        try (InputStream inputStream = connection.getInputStream()) {
-                                                try (ReadableByteChannel inputChannel = Channels.newChannel(inputStream)) {
-                                                        try (FileChannel outputChannel = FileChannel.open(artifactPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE)) {
-                                                                outputChannel.transferFrom(inputChannel, 0, Long.MAX_VALUE);
-                                                        }
-                                                }
-                                        }
-                                        return true;
-                                } else {
-                                        throw new IllegalStateException("Expected status code 200 but got " + connection.getResponseCode());
-                                }
+    // <editor-fold desc="Configuration Getters">
+    @Nonnull
+    public String getGameVersion() {
+        return this.gameVersion;
+    }
 
-                        } catch (IllegalStateException | IOException ex) {
-                                if (attempt == urls.length - 1) {
-                                        throw new MojoFailureException("Could not read/write artifact: " + ex.getMessage(), ex);
-                                }
-                        }
-                }
-                return false;
+    @Nonnull
+    public String getMappingVersion() {
+        return this.mappingVersion;
+    }
+
+    @Nonnull
+    public String getModule() {
+        return this.module;
+    }
+
+    @Nonnull
+    public String getPatchDirectory() {
+        return this.patchDirectory;
+    }
+
+    @Nonnull
+    public String getSourceDirectory() {
+        return this.sourceDirectory;
+    }
+
+    @Nonnull
+    public String getResourceDirectory() {
+        return this.resourceDirectory;
+    }
+    // </editor-fold>
+
+    /**
+     * "Wraps" a temporary file to ensure their deletion after processes involving its lifespan
+     * finish execution.
+     */
+    protected <E extends Exception> void temporary(@Nonnull PathConsumer<E> consumer) throws E, IOException {
+        Path tmp = Files.createTempFile("mvn_mc", "mc");
+
+        try {
+            consumer.accept(tmp);
+        } finally {
+            Files.delete(tmp);
         }
+    }
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void execute() throws MojoExecutionException, MojoFailureException {
-                if (!this.module.equalsIgnoreCase("client") && !this.module.equalsIgnoreCase("server")) {
-                        throw new MojoFailureException("Invalid module name \"" + module + "\"");
-                }
-        }
-
-        /**
-         * Executes a command based on the process builder specifications.
-         *
-         * @param builder a builder.
-         * @return a return value.
-         *
-         * @throws MojoFailureException when the execution fails.
-         */
-        protected int executeCommand(@Nonnull ProcessBuilder builder) throws MojoFailureException {
-                try {
-                        Process process = builder.start();
-                        int statusCode = process.waitFor();
-
-                        if (statusCode != 0) {
-                                try (InputStream inputStream = process.getErrorStream()) {
-                                        try (InputStreamReader reader = new InputStreamReader(inputStream)) {
-                                                try (BufferedReader bufferedReader = new BufferedReader(reader)) {
-                                                        String line;
-
-                                                        while ((line = bufferedReader.readLine()) != null) {
-                                                                this.getLog().error(line);
-                                                        }
-                                                }
-                                        }
-                                }
-                        }
-
-                        return statusCode;
-                } catch (IOException ex) {
-                        throw new MojoFailureException("Could not execute command: " + ex.getMessage(), ex);
-                } catch (InterruptedException ex) {
-                        throw new MojoFailureException("Interrupted while awaiting command result: " + ex.getMessage(), ex);
-                }
-        }
-
-        /**
-         * Generates an artifact descriptor.
-         *
-         * @param pomPath    a temporary descriptor path.
-         * @param artifactId an artifact identifier.
-         * @throws MojoFailureException when an error occurs while attempting to generate the descriptor.
-         */
-        protected void generateArtifactDescriptor(@Nonnull Path pomPath, @Nonnull String artifactId) throws MojoFailureException {
-                this.generateArtifactDescriptor(pomPath, artifactId, "");
-        }
-
-        /**
-         * Generates an artifact descriptor.
-         *
-         * @param pomPath       a temporary descriptor path.
-         * @param artifactId    an artifact identifier.
-         * @param versionSuffix a version suffix.
-         * @throws MojoFailureException when an error occurs while attempting to generate the descriptor.
-         */
-        protected void generateArtifactDescriptor(@Nonnull Path pomPath, @Nonnull String artifactId, @Nonnull String versionSuffix) throws MojoFailureException {
-                this.getLog().info("Generating maven artifact descriptor for net.minecraft:" + artifactId + ":" + this.gameVersion + versionSuffix);
-                Model model = new Model();
-
-                model.setGroupId(MINECRAFT_GROUP_ID);
-                model.setArtifactId(artifactId);
-                model.setVersion(this.gameVersion + versionSuffix);
-                model.setPackaging("jar");
-
-                Organization organization = new Organization();
-                organization.setName("Mojang");
-                organization.setUrl("http://mojang.com");
-                model.setOrganization(organization);
-
-                License license = new License();
-                license.setName("Mojang EULA");
-                license.setUrl("https://account.mojang.com/terms");
-                license.setDistribution("manual");
-                model.addLicense(license);
-
-                try (FileOutputStream outputStream = new FileOutputStream(pomPath.toFile())) {
-                        try (OutputStreamWriter writer = new OutputStreamWriter(outputStream)) {
-                                (new MavenXpp3Writer()).write(writer, model);
-                        }
-                } catch (IOException ex) {
-                        throw new MojoFailureException("Could not write model: " + ex.getMessage(), ex);
-                }
-        }
-
-        /**
-         * Instals an artifact to the local repository.
-         *
-         * @param artifactId    an artifact identifier.
-         * @param versionSuffix a version suffix.
-         * @param pomPath       a temporary descriptor path.
-         * @param artifactPath  a temporary artifact path.
-         * @return the generated artifact.
-         *
-         * @throws MojoFailureException when an error occurs while attempting to install the artifact.
-         */
-        protected Artifact installArtifact(@Nonnull String artifactId, @Nonnull String versionSuffix, @Nonnull Path pomPath, @Nonnull Path artifactPath) throws MojoFailureException {
-                this.getLog().info("Installing net.minecraft:" + artifactId + ":" + this.gameVersion + versionSuffix + " to local repository");
-                Artifact artifact = this.artifactFactory.createArtifact(MINECRAFT_GROUP_ID, artifactId, this.gameVersion + versionSuffix, "compile", "jar");
-
-                ArtifactMetadata metadata = new ProjectArtifactMetadata(artifact, pomPath.toFile());
-                artifact.addMetadata(metadata);
-
-                try {
-                        this.artifactInstaller.install(artifactPath.toFile(), artifact, this.session.getLocalRepository());
-
-                        try {
-                                Files.deleteIfExists(pomPath);
-                        } catch (IOException ex) {
-                                this.getLog().warn("Could not delete generated module descriptor: " + ex.getMessage(), ex);
-                                this.getLog().warn("You may want to delete " + pomPath.toAbsolutePath().toString() + " manually.");
-                        }
-
-                        try {
-                                Files.deleteIfExists(artifactPath);
-                        } catch (IOException ex) {
-                                this.getLog().warn("Could not delete module artifact: " + ex.getMessage(), ex);
-                                this.getLog().warn("You may want to delete " + pomPath.toAbsolutePath().toString() + " manually");
-                        }
-
-                        return artifact;
-                } catch (ArtifactInstallationException ex) {
-                        throw new MojoFailureException("Could not install artifact: " + ex.getMessage(), ex);
-                }
-        }
-
-        /**
-         * Installs an artifact to the local repository.
-         *
-         * @param artifactId   an artifact identifier.
-         * @param pomPath      a temporary descriptor path.
-         * @param artifactPath a temporary artifact path.
-         * @return the generated artifact.
-         *
-         * @throws MojoFailureException when an error occurs while attempting to install the artifact.
-         */
-        protected Artifact installArtifact(@Nonnull String artifactId, @Nonnull Path pomPath, @Nonnull Path artifactPath) throws MojoFailureException {
-                return this.installArtifact(artifactId, "", pomPath, artifactPath);
-        }
-
-        /**
-         * Locates an artifact in the local repository.
-         *
-         * @param artifactId    an artifact identifier.
-         * @param versionSuffix a version suffix.
-         * @return an artifact or, if no local version was found, an empty optional.
-         *
-         * @throws ArtifactResolutionException when the artifact resolution fails.
-         */
-        protected Optional<Artifact> locateArtifact(@Nonnull String artifactId, @Nonnull String versionSuffix) throws ArtifactResolutionException {
-                try {
-                        Artifact artifact = this.artifactFactory.createBuildArtifact(MINECRAFT_GROUP_ID, artifactId, this.gameVersion + versionSuffix, "jar");
-                        this.artifactResolver.resolve(artifact, Collections.emptyList(), this.session.getLocalRepository());
-                        return Optional.of(artifact);
-                } catch (ArtifactNotFoundException ex) {
-                        return Optional.empty();
-                }
-        }
-
-        /**
-         * Locates an artifact in the local repository.
-         *
-         * @param artifactId an artifact identifier.
-         * @return an artifact or, if no local version was found, an empty optional.
-         *
-         * @throws ArtifactResolutionException when the artifact resolution fails.
-         */
-        @Nonnull
-        protected Optional<Artifact> locateArtifact(@Nonnull String artifactId) throws ArtifactResolutionException {
-                return this.locateArtifact(artifactId, "");
-        }
+    /**
+     * Provides a simple consumer which is capable of throwing any kind of exception.
+     *
+     * @param <E> an exception type.
+     */
+    @FunctionalInterface
+    public interface PathConsumer<E extends Exception> {
+        void accept(@Nonnull Path p) throws E;
+    }
 }
