d9303582276c0678c089732cdabab853e63d1100
Johannes Donath <johannesd@torchmind.com>
2016-12-20 06:28:40 +0100

    Added a constant to represent the MCP live version within the local repository.

--- src/main/java/org/basinmc/maven/plugins/minecraft/AbstractArtifactMojo.java
+++ src/main/java/org/basinmc/maven/plugins/minecraft/AbstractArtifactMojo.java
@@ -67,6 +67,7 @@ public abstract class AbstractArtifactMojo extends AbstractMinecraftMojo {
     protected static final String MINECRAFT_GROUP_ID = BASE_GROUP_ID + ".minecraft";
     protected static final String SRG_ARTIFACT_ID = "mappings-srg";
     protected static final String MCP_ARTIFACT_ID = "mappings-mcp";
+    protected static final String MCP_LIVE_VERSION = "0.0.0-SNAPSHOT";
     protected static final String VANILLA_CLASSIFIER = "vanilla";
     protected static final String MAPPED_CLASSIFIER = "mapped";
     protected static final String SOURCE_CLASSIFIER = "source";
