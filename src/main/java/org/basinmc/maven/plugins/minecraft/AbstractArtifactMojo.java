e27fd06df7858a2a1ff4d72d230792e0c5c472b3
Johannes Donath <johannesd@torchmind.com>
2016-12-20 05:23:55 +0100

    Added an artifact constant for MCP mappings.

--- src/main/java/org/basinmc/maven/plugins/minecraft/AbstractArtifactMojo.java
+++ src/main/java/org/basinmc/maven/plugins/minecraft/AbstractArtifactMojo.java
@@ -62,6 +62,7 @@ public abstract class AbstractArtifactMojo extends AbstractMinecraftMojo {
 
     protected static final String MINECRAFT_GROUP_ID = BASE_GROUP_ID + ".minecraft";
     protected static final String SRG_ARTIFACT_ID = "mappings-srg";
+    protected static final String MCP_ARTIFACT_ID = "mappings-mcp";
     protected static final String VANILLA_CLASSIFIER = "vanilla";
     protected static final String MAPPED_CLASSIFIER = "mapped";
     protected static final String SOURCE_CLASSIFIER = "source";
