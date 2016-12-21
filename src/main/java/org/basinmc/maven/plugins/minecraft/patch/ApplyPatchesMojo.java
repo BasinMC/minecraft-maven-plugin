5f5265c816620c1ee5acb20ae809e98378be8720
Johannes Donath <johannesd@torchmind.com>
2016-12-21 11:22:39 +0100

    Added a mojo to apply a set of patches to the codebase.

---
+++ src/main/java/org/basinmc/maven/plugins/minecraft/patch/ApplyPatchesMojo.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.maven.plugins.minecraft.patch;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Applies a set of patches from the source directory.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+@Mojo(
+        name = "apply-patches",
+        requiresProject = false,
+        threadSafe = true,
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES
+)
+public class ApplyPatchesMojo extends AbstractGitCommandMojo {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        super.execute();
+
+        this.getLog().info("Applying patches");
+
+        // abort any pending merges before attempting anything else
+        try {
+            if (this.execute(new ProcessBuilder("git", "am", "--abort")) != 0) {
+                throw new MojoFailureException("Git terminated with an unexpected error");
+            }
+        } catch (InterruptedException ex) {
+            throw new MojoFailureException("Interrupted while awaiting git return status: " + ex.getMessage(), ex);
+        } catch (IOException ex) {
+            throw new MojoFailureException("Failed to invoke git: " + ex.getMessage(), ex);
+        }
+
+        if (this.getSettings().isInteractiveMode()) {
+            this.getLog().info("Interactive mode is enabled - Conflicts will be reported and may pause the build process");
+            this.getLog().info("Note: If you are running this build on a CI server, you should disable interactive mode in your maven configuration (or switch to batch mode using the corresponding command line argument)");
+        }
+
+        Path workingPath = Paths.get(".").toAbsolutePath();
+
+        // apply patches one-by-one
+        try {
+            try {
+                Files.walk(this.getPatchDirectory().toPath())
+                        .filter((p) -> p.getFileName().endsWith(".patch"))
+                        .forEach((p) -> {
+                            this.getLog().info("Applying " + p.toString());
+                            List<String> command = new ArrayList<>(Arrays.asList("git", "am", "--ignore-whitespace", "--3way", workingPath.relativize(p.toAbsolutePath()).toString()));
+
+                            if (this.getSettings().isInteractiveMode()) {
+                                command.add("--reject");
+                            }
+
+                            try {
+                                if (this.execute(new ProcessBuilder(command)) != 0) {
+                                    this.getLog().error("Could not apply patch from file " + p.toString());
+
+                                    if (!this.getSettings().isInteractiveMode()) {
+                                        this.getLog().error("Cannot recover from failure - Switch to interactive mode to resolve this issue");
+                                        throw new MojoFailureException("Failed to apply patch " + p.toString());
+                                    }
+
+                                    this.getLog().info("Merge mode activated");
+                                    this.getLog().info("Perform a manual merge for the modified files");
+
+                                    while (true) {
+                                        System.out.println("Continue? [Y/N]");
+                                        int input = System.in.read();
+
+                                        if (input == 'N' || input == 'n') {
+                                            this.getLog().error("Did not solve merge error - Cannot recover from build failure");
+                                            throw new MojoFailureException("Failed to apply patch " + p.toString());
+                                        }
+
+                                        if (input == 'Y' || input == 'y') {
+                                            break;
+                                        }
+                                    }
+                                }
+                            } catch (Exception ex) {
+                                throw new WrappedException(ex);
+                            }
+                        });
+            } catch (WrappedException ex) {
+                throw ex.getCause();
+            }
+        } catch (IOException ex) {
+            throw new MojoFailureException("Cannot access one or more patch files: " + ex.getMessage(), ex);
+        } catch (MojoFailureException ex) {
+            throw ex;
+        } catch (Throwable ex) {
+            throw new MojoFailureException("Caught unexpected exception: " + ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Provides a wrapper exception which acts as a transport through the boundaries of lambda
+     * methods.
+     */
+    private static class WrappedException extends RuntimeException {
+        public WrappedException(Throwable throwable) {
+            super(throwable);
+        }
+    }
+}
